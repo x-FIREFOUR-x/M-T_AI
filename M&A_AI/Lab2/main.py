c5498873694d0a9963a0cb754fd10ebf6503620f
Alex <olexandrpasalsky@gmail.com>
2022-11-02 00:45:23 +0200

    clean code

--- M&A_AI/Lab2/main.py
+++ M&A_AI/Lab2/main.py
@@ -12,23 +12,21 @@ def triangle(x, a, b, c):
     plt.title(f"trimf(x,P) P=[{a}, {b}, {c}]")
     plt.show()
 
-def gauss(x, z, G, name_f, index):
-    f = fuzz.gaussmf(x, z, G)
-    plt.plot(x, f)
-    plt.title(f"gauss(x,P) P=[{z}, {G}]")
 
-    plt.xlim(0, 0.1)
-    plt.ylim(0)
-    plt.xticks(np.arange(0, 1.1, step=0.1))
-    plt.yticks(np.arange(0, 1.1, step=0.5))
-    plt.grid(color='#CCCCCC', linestyle='-')
+def membership_function_gauss(amount_function, G, name):
+    def gauss(z, G, index):
+        f = fuzz.gaussmf(x, z, G)
+        plt.plot(x, f)
 
-    plt.text(z, 1.025, name_f + str(index), fontsize=10)
-    plt.plot([z], [1], 'o', zorder=2)
+        plt.xlim(0, 0.1)
+        plt.ylim(0)
+        plt.xticks(np.arange(0, 1.1, step=0.1))
+        plt.yticks(np.arange(0, 1.1, step=0.5))
+        plt.grid(color='#CCCCCC', linestyle='-')
 
+        plt.text(z, 1.025, name + str(index), fontsize=10)
+        plt.plot([z], [1], 'o', zorder=2)
 
-
-def membership_function(amount_function, G, name, function):
     x = np.arange(0, 1.02, 0.0025)
     step = 1 / (amount_function -1)
 
@@ -36,9 +34,10 @@ def membership_function(amount_function, G, name, function):
     current_max_point = 0
     max_points = []
     for i in range(1, amount_function + 1):
-        function(x, current_max_point, G, name, i)
+        gauss(current_max_point, G, i)
         max_points.append(current_max_point)
         current_max_point += step
+    plt.title(f"Membership function {name}")
     plt.show()
     return max_points
 
@@ -53,6 +52,7 @@ def modeling_function():
     X, Y = np.meshgrid(x, y)
     Z = function_z(X, Y)
     fig = plt.figure()
+    plt.title("function for modeling: z = x * cos|y|")
     ax = plt.axes(projection='3d')
     ax.contour3D(X, Y, Z, 100, cmap='viridis')
     ax.set_xlabel('x')
@@ -101,9 +101,9 @@ def table_name_mf_for_mx_my(table_value, mf):
 
 
 if __name__ == '__main__':
-    mx = membership_function(6, 0.0850, "mx", gauss)
-    my = membership_function(6, 0.0850, "my", gauss)
-    mf = membership_function(9, 0.053, "mf", gauss)
+    mx = membership_function_gauss(6, 0.0850, "mx")
+    my = membership_function_gauss(6, 0.0850, "my")
+    mf = membership_function_gauss(9, 0.053, "mf")
 
     modeling_function()
 
