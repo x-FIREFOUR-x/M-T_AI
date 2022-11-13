b65da54266acdb80524b5f3ebdb4eacf58b29ee2
Alex <olexandrpasalsky@gmail.com>
2022-11-13 18:01:53 +0200

    generation set points

--- M&A_AI/Lab3/main.py
+++ M&A_AI/Lab3/main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import numpy as np
+import random as rand
+import matplotlib.pyplot as plt
+import skfuzzy as fuzz
 
 
-# Press the green button in the gutter to run the script.
+def random_generate_points(amount, min_limit, max_limit):
+    points = []
+    for i in range(0, amount):
+        points.append([rand.randint(min_limit, max_limit), rand.randint(min_limit, max_limit)])
+    return points
+
+
+def visualize_points(points, min_limit, max_limit):
+    plt.figure(figsize=(6, 6))
+    plt.scatter(points[:, 0], points[:, 1], s=15)
+
+    plt.xlim(0)
+    plt.ylim(0)
+    plt.xticks(np.arange(min_limit, max_limit + 1, step=1))
+    plt.yticks(np.arange(min_limit, max_limit + 1, step=1))
+
+    plt.title(f"Set {len(points)} points")
+
+    plt.show()
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    points = random_generate_points(30, 1, 15)
+    points = np.array(points)
+    print(points)
+    visualize_points(points, 0, 15)
