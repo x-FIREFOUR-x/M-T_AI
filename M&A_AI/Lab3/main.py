7af7be7b40d5883dffdabf96a764e7b151933081
Alex <olexandrpasalsky@gmail.com>
2022-11-13 18:51:18 +0200

    fcm model. view clusters

--- M&A_AI/Lab3/main.py
+++ M&A_AI/Lab3/main.py
@@ -2,6 +2,9 @@ import numpy as np
 import random as rand
 import matplotlib.pyplot as plt
 import skfuzzy as fuzz
+from fcmeans import FCM
+
+
 
 
 def random_generate_points(amount, min_limit, max_limit):
@@ -13,7 +16,7 @@ def random_generate_points(amount, min_limit, max_limit):
 
 def visualize_points(points, min_limit, max_limit):
     plt.figure(figsize=(6, 6))
-    plt.scatter(points[:, 0], points[:, 1], s=15)
+    plt.scatter(points[:, 0], points[:, 1], marker=".", s=100)
 
     plt.xlim(0)
     plt.ylim(0)
@@ -25,9 +28,33 @@ def visualize_points(points, min_limit, max_limit):
     plt.show()
 
 
+def visualize_clusters(points, labels, centers, min_limit, max_limit):
+    plt.figure(figsize=(6, 6))
+    plt.scatter(points[:, 0], points[:, 1], marker=".", s=100, c=labels, cmap='brg')
+    plt.scatter(centers[:, 0], centers[:, 1], marker="x", s=100, c=range(len(centers)), cmap='brg')
+
+    plt.xlim(0)
+    plt.ylim(0)
+    plt.xticks(np.arange(min_limit, max_limit + 1, step=1))
+    plt.yticks(np.arange(min_limit, max_limit + 1, step=1))
+
+    plt.title(f"{len(centers)} Clusters points's")
+
+    plt.show()
+
 
 if __name__ == '__main__':
-    points = random_generate_points(30, 1, 15)
-    points = np.array(points)
+    amount_points = 30
+    min_limit = 0
+    max_limit = 15
+    amount_clusters = 3
+
+    points = np.array(random_generate_points(amount_points, min_limit, max_limit))
     print(points)
-    visualize_points(points, 0, 15)
+    visualize_points(points, min_limit, max_limit)
+
+    model = FCM(n_clusters=amount_clusters)
+    model.fit(points)
+    labels = model.predict(points)
+    centers = model.centers
+    visualize_clusters(points, labels, centers, min_limit, max_limit)
